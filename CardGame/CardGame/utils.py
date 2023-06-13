55d3cd6efa4bd39f5843e47dbce43de7097a2cba
A-baoYang <rubyyang0615@gmail.com>
2023-06-13 23:29:25 +0800

    [CardGame] Update function

--- CardGame/CardGame/utils.py
+++ CardGame/CardGame/utils.py
@@ -2,6 +2,9 @@ import logging
 import os
 from datetime import datetime
 from pathlib import Path
+from typing import Any
+import json
+import pandas as pd
 
 
 def log_setting(
@@ -35,3 +38,26 @@ def check_input_valid(x: str):
     except Exception as e:
         return False
     return True
+
+
+def read_data(path: str) -> Any:
+    if path.endswith(".json"):
+        with open(path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+    elif path.endswith(".txt"):
+        with open(path, "r", encoding="utf-8") as f:
+            data = f.read()
+        data = data.split("\n")
+    elif path.endswith(".csv"):
+        data = pd.read_csv(path)
+    elif path.endswith(".ndjson"):
+        data = pd.read_json(path, lines=True, orient="records")
+    elif path.endswith(".ndjson.gz"):
+        data = pd.read_json(path, lines=True, orient="records", compression="gzip")
+    elif path.endswith(".pickle"):
+        data = pd.read_pickle(path)
+    elif path.endswith("parquet"):
+        data = pd.read_parquet(path)
+    else:
+        data = []
+    return data
